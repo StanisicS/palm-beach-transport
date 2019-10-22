29e7ee58d62c1140da7ab37250b0e97a0eca4845
StanisicS <stevan.stanisic@outlook.com>
2019-10-22 18:52:04 +0200

    Initial Commit

---
+++ src/pages/about.js
@@ -0,0 +1,235 @@
+import React from "react"
+import { graphql } from "gatsby"
+import {
+  Box,
+  //Icon,
+  Grid,
+  //Avatar,
+  makeStyles,
+  //Divider,
+  Container,
+} from "@material-ui/core"
+import {
+  Title,
+  Text,
+  DividedSection,
+  TabPill,
+} from "gatsby-theme-material-foundry"
+import Img from "gatsby-image"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Dashboard from "@material-ui/icons/People"
+import Schedule from "@material-ui/icons/Business"
+import List from "@material-ui/icons/List"
+import Description from "@material-ui/icons/Description"
+//import { AvatarCard, CompanyCard } from "../components/custom-cards"
+
+const useStyles = makeStyles({
+  avatar: {
+    margin: 10,
+  },
+  bigAvatar: {
+    width: 100,
+    height: 100,
+    marginRight: "24px",
+  },
+})
+
+function AboutPage({ data }) {
+  const classes = useStyles()
+
+  return (
+    <Layout>
+      <SEO title="About Palm Beach Transport" />
+      <DividedSection
+        black
+        height="50vh"
+        image={data.cover.childImageSharp.fixed.src}
+        backgroundBlendMode="overlay"
+        backgroundColor="#333333"
+      >
+        <Title variant="h2" align="center">
+          About Palm Beach Tranmsport
+        </Title>
+      </DividedSection>
+      <Container maxWidth="md">
+        <TabPill
+          alignCenter
+          color="primary"
+          tabs={[
+            {
+              tabButton: "About Us",
+              tabIcon: Description,
+              tabContent: (
+                <>
+                  <Box p={6}>
+                    <Title variant="h3" align="center" gutterBottom>
+                      Palm Beach Transport LLC
+                    </Title>
+                    <Text variant="body1" gutterBottom>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      <ul>
+                        <li>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt.
+                        </li>
+                        <li>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt.
+                        </li>
+                        <li>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt.
+                        </li>
+                      </ul>
+                    </Text>
+                  </Box>
+                </>
+              ),
+            },
+            {
+              tabButton: "Team",
+              tabIcon: Dashboard,
+              tabContent: (
+                <>
+                  <Container maxWidth="md">
+                    <Box p={6}>
+                      <Title variant="h3" align="center" gutterBottom>
+                        Team
+                      </Title>
+                      <Text variant="h5" align="center" gutterBottom>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua..
+                      </Text>
+                    </Box>
+                  </Container>
+                  {/* <Grid container>
+                    {data.people &&
+                      data.people.nodes.map(person => (
+                        <Grid item xs={6} md={4}>
+                          <AvatarCard
+                            img={person.avatarUrl}
+                            name={person.fullname}
+                            role={person.kabisrole}
+                            logo={person.company.logoUrl}
+                            orgRole={person.orgrole}
+                          />
+                        </Grid>
+                      ))}
+                  </Grid> */}
+                </>
+              ),
+            },
+            {
+              tabButton: "Partners",
+              tabIcon: List,
+              tabContent: (
+                <>
+                  <Container maxWidth="md">
+                    <Box p={6}>
+                      <Title variant="h3" align="center" gutterBottom>
+                        Partners
+                      </Title>
+                    </Box>
+                  </Container>
+                  <Grid container spacing={2}>
+                    {data.partners &&
+                      data.partners.nodes.map(company => (
+                        <Grid item xs={6} md={4}>
+                          {/* <CompanyCard
+                            logo={company.logoUrl}
+                            name={`${company.name}`}
+                            website={company.website}
+                          /> */}
+                        </Grid>
+                      ))}
+                  </Grid>
+                </>
+              ),
+            },
+            // {
+            //   tabButton: "Members",
+            //   tabIcon: Schedule,
+            //   tabContent: (
+            //     <>
+            //       <Container maxWidth="md">
+            //         <Box p={6}>
+            //           <Title variant="h3" align="center" gutterBottom>
+            //             industry Members
+            //           </Title>
+            //         </Box>
+            //       </Container>
+            //       <Grid container spacing={2}>
+            //         {data.industry &&
+            //           data.industry.nodes.map(company => (
+            //             <Grid item xs={6} md={4}>
+            //               {/* <CompanyCard
+            //                 logo={company.logoUrl}
+            //                 name={`${company.name}`}
+            //                 website={company.website}
+            //               /> */}
+            //             </Grid>
+            //           ))}
+            //       </Grid>
+            //     </>
+            //   ),
+            // },
+          ]}
+        />
+      </Container>
+    </Layout>
+  )
+}
+export const ItemPageQuery = graphql`
+  query People {
+    people: allGoogleSheetPeopleRow(sort: { fields: fullname, order: ASC }) {
+      nodes {
+        fullname
+        kabisrole
+        avatarUrl
+        orgrole
+        company {
+          logoUrl
+          name
+        }
+      }
+    }
+    partners: allGoogleSheetCompaniesRow(
+      filter: { partnertype: { eq: "Partner" } }
+    ) {
+      nodes {
+        name
+        website
+        logoUrl
+      }
+    }
+    industry: allGoogleSheetCompaniesRow(
+      filter: { partnertype: { eq: "Industry Member" } }
+    ) {
+      nodes {
+        name
+        website
+        logoUrl
+      }
+    }
+    cover: file(relativePath: { eq: "kabis-kickoff.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 1920, height: 1080) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
+
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,159 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+import {
+  DividedSection,
+  Title,
+  Text,
+  Button,
+} from "gatsby-theme-material-foundry"
+import { Container, Box } from "@material-ui/core"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+function IndexPage(props) {
+  const img = props.data.cover.childImageSharp.fixed.src
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <DividedSection
+        black
+        image={img}
+        height="100vh"
+        backgroundRepeat="no-repeat"
+        backgroundPosition="center center"
+        backgroundSize="cover"
+        backgroundAttachment="fixed"
+        backgroundBlendMode="overlay"
+        backgroundColor="#333333"
+      >
+        <Container maxWidth="sm" align="center">
+          <Title variant="h3" align="center">
+            Lorem Ipsum
+          </Title>
+          <Text variant="h5" align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Text>
+          <Box m={2}>
+            <Button color="primary" to="/SignUp">
+              Register
+            </Button>
+          </Box>
+        </Container>
+      </DividedSection>
+      {/* <DividedSection>
+        {props.data.partners &&
+          props.data.partners.nodes.map(company => (
+            <img src={company.logoUrl} width="100px" />
+          ))}
+      </DividedSection> */}
+      <DividedSection height="80vh">
+        <Img fluid={props.data.kickoff.childImageSharp.fluid} />
+        <Container>
+          <Title variant="h4" align="left">
+            Lorem Ipsum
+          </Title>
+          <Text variant="body1" align="left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Text>
+        </Container>
+      </DividedSection>
+      {/* <DividedSection height="50vh" info>
+        <Box>
+          <Title variant="h5" align="center">
+            Education
+          </Title>
+          <Text variant="body1">
+            Strenthen the educational offerings within sustainable aquaculture
+            in the bergen region
+          </Text>
+        </Box>
+        <Box>
+          <Title variant="h5">Mobility</Title>
+          <Text variant="body1">
+            facilitate increased mobility for students and researcher between
+            RnD institutions
+          </Text>
+        </Box>
+        <Box>
+          <Title variant="h5">Knowledge</Title>
+          <Text variant="body1">
+            The KABIS project aim to focus and strengthen
+          </Text>
+        </Box>
+        <Box>
+          <Title variant="h5">Innovations</Title>
+          <Text variant="body1">
+            The KABIS project aim to focus and strengthen
+          </Text>
+        </Box>
+      </DividedSection> */}
+      <Container maxWidth="md">
+        <Box pt={8}>
+          <Text align="center" variant="h4">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt."
+          </Text>
+        </Box>
+        <Box justifyContent="flex-end" mr={8}>
+          {/* <AvatarCard
+            horizontal
+            img={props.data.quote.avatarUrl}
+            name={props.data.quote.fullname}
+            role={props.data.quote.kabisrole}
+            logo={props.data.quote.company.logoUrl}
+            orgRole={props.data.quote.orgrole}
+          /> */}
+        </Box>
+      </Container>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexPage {
+    cover: file(relativePath: { eq: "cover-main.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 1920, height: 1080) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    kickoff: file(relativePath: { eq: "pbt-kickoff.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 1000, maxHeight: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    partners: allGoogleSheetCompaniesRow(
+      filter: { partnertype: { eq: "Partner" } }
+    ) {
+      nodes {
+        name
+        website
+        logoUrl
+      }
+    }
+    quote: googleSheetPeopleRow(fullname: { eq: "Sigurd Handeland" }) {
+      fullname
+      kabisrole
+      avatarUrl
+      orgrole
+      company {
+        logoUrl
+        name
+      }
+    }
+  }
+`
