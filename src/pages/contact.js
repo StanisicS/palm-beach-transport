3f502cd073d89fbb68e65a8f513bd3b8d47a3493
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-20 23:33:28 +0100

    forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,37 @@
+import withRoot from "../modules/withRoot"
 import React from "react"
 import Layout from "../components/layout"
+import { makeStyles } from "@material-ui/core/styles"
+import Typography from "../modules/components/Typography"
 
-const ContactFormPage = () => (
+const useStyles = makeStyles(theme => ({
+  form: {
+    marginTop: theme.spacing(6),
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(2),
+  },
+}))
+
+const ContactFormPage = useStyles => (
   <Layout>
-    <h1>Contact</h1>
+    <Typography>
+      <h1>Contact</h1>
 
-    <form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form" />
-      <div>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label>Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Send</button>
-    </form>
+      <form name="Contact Form" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="Contact Form" />
+        <div>
+          <label>Your Email:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>Message:</label>
+          <textarea name="message" />
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </Typography>
   </Layout>
 )
-export default ContactFormPage
+export default withRoot(ContactFormPage)
