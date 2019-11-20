02f7fb8aba38bd05339212e4d798bb69ec4ec523
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-20 17:59:26 +0100

    footer

--- src/modules/views/AppFooter.js
+++ src/modules/views/AppFooter.js
@@ -1,54 +1,54 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
-import Container from '@material-ui/core/Container';
-import Typography from '../components/Typography';
-import TextField from '../components/TextField';
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import Link from "@material-ui/core/Link"
+import Container from "@material-ui/core/Container"
+import Typography from "../components/Typography"
+import TextField from "../components/TextField"
 
 function Copyright() {
   return (
     <React.Fragment>
-      {'Â© '}
+      {"Â© "}
       <Link color="inherit" href="https://material-ui.com/">
         www.evansit.rs
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
     </React.Fragment>
-  );
+  )
 }
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex",
     backgroundColor: theme.palette.secondary.light,
   },
   container: {
     marginTop: theme.spacing(8),
     marginBottom: theme.spacing(8),
-    display: 'flex',
+    display: "flex",
   },
   iconsWrapper: {
     height: 120,
   },
   icons: {
-    display: 'flex',
+    display: "flex",
   },
   icon: {
     width: 48,
     height: 48,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: theme.palette.warning.main,
     marginRight: theme.spacing(1),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.warning.dark,
     },
   },
   list: {
     margin: 0,
-    listStyle: 'none',
+    listStyle: "none",
     paddingLeft: 0,
   },
   listItem: {
@@ -59,21 +59,21 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(1),
     width: 150,
   },
-}));
+}))
 
 const LANGUAGES = [
   {
-    code: 'en-US',
-    name: 'English',
+    code: "en-US",
+    name: "English",
   },
   {
-    code: 'fr-FR',
-    name: 'FranÃ§ais',
+    code: "fr-FR",
+    name: "FranÃ§ais",
   },
-];
+]
 
 export default function AppFooter() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <Typography component="footer" className={classes.root}>
@@ -137,14 +137,19 @@ export default function AppFooter() {
             </Typography>
             <ul className={classes.list}>
               <li className={classes.listItem}>
-                <Link href="/premium-themes/onepirate/terms/">support@palmbeachtrans.com</Link>
+                <Link href="/premium-themes/onepirate/terms/">
+                  support@palmbeachtrans.com
+                </Link>
               </li>
               <li className={classes.listItem}>
-                <Link href="/premium-themes/onepirate/privacy/">Contact us: 561-264-3410</Link>
+                <Link href="/premium-themes/onepirate/privacy/">
+                  Contact us: 561-264-3410
+                </Link>
               </li>
             </ul>
-            </Grid>
           </Grid>
+        </Grid>
       </Container>
-      </Typography>
+    </Typography>
   )
+}
