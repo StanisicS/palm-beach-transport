371791499f82da6c3308ef401090940eef060a66
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-20 23:49:43 +0100

    again

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,25 @@
 import withRoot from "../modules/withRoot"
 import React from "react"
 import Layout from "../components/layout"
-import { makeStyles } from "@material-ui/core/styles"
-import Typography from "../modules/components/Typography"
+import AppForm from "../modules/views/AppForm"
 
 const ContactFormPage = () => (
   <Layout>
-    <Typography>
-      <h1>Contact</h1>
+    <AppForm />
+    <h1>Contact</h1>
 
-      <form name="Contact Form" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="Contact Form" />
-        <div>
-          <label>Your Email:</label>
-          <input type="email" name="email" />
-        </div>
-        <div>
-          <label>Message:</label>
-          <textarea name="message" />
-        </div>
-        <button type="submit">Send</button>
-      </form>
-    </Typography>
+    <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <label>Your Email:</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+      </div>
+      <button type="submit">Send</button>
+    </form>
   </Layout>
 )
 export default withRoot(ContactFormPage)
