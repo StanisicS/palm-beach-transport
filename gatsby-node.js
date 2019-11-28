a0574b1d55926664fd9a4dd94397901dae1eb3e3
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-28 20:27:05 +0100

    config

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,35 +2,40 @@ const path = require("path")
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
-      const postTemplate = path.resolve("src/templates/postTemplate.js")
-    // to create the page we need access to the blog post template
+  const postTemplate = path.resolve("src/templates/postTemplate.js")
+  // to create the page we need access to the blog post template
 
-   return graphql (`{    
-     allMarkdownRemark(      
-       sort: { order: DESC, fields: [frontmatter___date] }      
-       limit: 1000    ) {      
-         edges {        
-           node {          
-             excerpt(pruneLength: 250)          
-             html          
-             id          
-             frontmatter {            
-               date            
-               path            
-               title          
-              }        
-            }      
-          }    
-        }  
-      }`
-      )    
-      .then(result => {      
-        if (result.errors) {        
-          return Promise.reject(result.errors);
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            excerpt(pruneLength: 250)
+            html
+            id
+            frontmatter {
+              date
+              path
+              title
+            }
+          }
+        }
       }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
 
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {          
-        createPage({            
-          path: node.frontmatter.path,            
-          component: postTemplate,            
-          context: {}
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+        context: {},
+      })
+    })
+  })
+}
