594315137b768e58ec94b8c40ae49de8d3387b14
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-29 04:58:20 +0100

    100

--- gatsby-node.js
+++ gatsby-node.js
@@ -76,6 +76,19 @@ const path = require("path")
 //     })
 //   })
 // }
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
 exports.createPages = async ({ graphql, actions, reporter }) => {
   // **Note:** The graphql function call returns a Promise
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
