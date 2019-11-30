3e85d22e58c184472172f6a6cd983e1bd7060958
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-30 08:18:25 +0100

    zzzz

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,20 +1,5 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
-import styled from "styled-components"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import image from "../components/image"
-
-const Kanta = styled.div`
-  margin: 4rem 8ren;
-  padding: 1px;
-  max-width: 600px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -22,21 +7,16 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <Layout>
-      <MDBContainer>
-        <Kanta className="blog-post-container">
-          <Kanta className="blog-post">
-            <h1>{frontmatter.title}</h1>
-            <h2>{frontmatter.date}</h2>
-            <Image />
-            <Kanta
-              className="blog-post-content"
-              dangerouslySetInnerHTML={{ __html: html }}
-            />
-          </Kanta>
-        </Kanta>
-      </MDBContainer>
-    </Layout>
+    <div className="blog-post-container">
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
   )
 }
 
