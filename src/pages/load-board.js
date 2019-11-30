a39f210a3311c587bf0bf2e36d2eef995608576b
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-30 07:17:25 +0100

    jako dobro

--- src/pages/load-board.js
+++ src/pages/load-board.js
@@ -1,6 +1,20 @@
 import React from "react"
 import { graphql } from "gatsby"
 import PostLink from "../components/post-link"
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
+import styled from "styled-components"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Kanta = styled.div`
+  margin: 4rem auto;
+  padding: 1px;
+  max-width: 900px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 const LoadBoard = ({
   data: {
@@ -11,7 +25,8 @@ const LoadBoard = ({
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-  return <div>{Posts}</div>
+  return
+  ;<Kanta>{Posts}</Kanta>
 }
 
 export default LoadBoard
