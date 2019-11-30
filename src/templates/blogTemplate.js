01fd23e8f3cf536df53fc2a446c41fbdd01723c2
Stevan Stanisic <stevan.stanisic@outlook.com>
2019-11-30 07:14:57 +0100

    ne mogu da t opisem

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,5 +1,19 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
+import styled from "styled-components"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Kanta = styled.div`
+  margin: 4rem auto;
+  padding: 1px;
+  max-width: 900px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -7,16 +21,22 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+    <Layout>
+      <Kanta>
+        <MDBContainer>
+          <div className="blog-post-container">
+            <div className="blog-post">
+              <h1>{frontmatter.title}</h1>
+              <h2>{frontmatter.date}</h2>
+              <div
+                className="blog-post-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </div>
+          </div>
+        </MDBContainer>
+      </Kanta>
+    </Layout>
   )
 }
 
